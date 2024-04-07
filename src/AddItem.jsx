38319a925877bc737b4e427fbd01dcd2846e113e
hemangini-gohel <gohelhemangi9719@gmail.com>
2024-04-07 17:40:48 +0530

    1.useRef hook

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,24 +1,22 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 /* eslint-disable react/prop-types */
 const AddItem = ({ submitForm }) => {
-  const [itemInput, setItemInput] = useState("");
-  const handleInputChange = (value) => {
-    setItemInput(value);
-  };
+  const itemElem = useRef("");
   const handleSubmit = (value) => {
     event.preventDefault();
     submitForm(value);
-    setItemInput("");
   };
   return (
     <>
-      <form className="d-flex my-3" onSubmit={() => handleSubmit(itemInput)}>
+      <form
+        className="d-flex my-3"
+        onSubmit={() => handleSubmit(itemElem.current.value)}
+      >
         <input
           placeholder="enter tasks here"
           className="form-control task-input"
-          onChange={() => handleInputChange(event.target.value)}
-          value={itemInput}
+          ref={itemElem}
         ></input>
         <button type="submit" className="btn btn-warning fw-bold">
           Create
